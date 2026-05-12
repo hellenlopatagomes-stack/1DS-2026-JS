46fe63945d453468e12e17d4d48374d2cde2ca29
Hellen <hellen.lopata.gomes@escola.pr.gov.br>
2026-05-12 10:29:30 -0300

    Add files via upload

--- aula01.js
+++ aula01.js
@@ -1,1 +1,9 @@
-alert("oncarpa capim fica batendo no amiguinho!!!")
+//alert("o carpa capim fica batendo no amiguinho!!!")
+
+console.log("primeiro código em JS");
+console.log(4+5);
+console.log(3 * 4);
+console.log( true );
+console.log( 3.89 );
+
+console.log( "Hellen" + " " +23+" " + 1.83 " "+ true );
