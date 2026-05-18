48594723a64b65e2ad9b5f96823eee101fccafac
Hellen <hellen.lopata.gomes@escola.pr.gov.br>
2026-05-18 09:21:37 -0300

    Add files via upload

---
+++ 1DS-2026-JS/aula.02.js
@@ -0,0 +1,47 @@
+//DEFINIÇÃO DE VARIÁVEIS
+
+let pessoa = "Nome da pessoa";
+let idadePessoa = 30;
+let alturaPessoa = 1.93; 
+
+
+
+
+
+
+
+
+let salarioTotal = salarioBonus + salarioPessoal;
+
+console.log( pessoa ); 
+console.log( idadePessoa );
+console.log( alturaPessoa);
+console.log( statusPessoa );
+console.log( salarioPessoa );
+console.log( salarioBonus );
+console.log( salarioTotal);
+
+//UNINDOS TODAS AS INFORMAÇÕES EM UM TEXTO:
+console.log( 
+'o nome digitado foi:  '+pessoa+
+' a idade digitada foi: '+idadePessoa+
+' a altura digitada foi:  '+alturaPessoa+
+' o status da pessoa é:  '+statusPessoa+
+' o salário da pessoa é:  'salarioPessoa+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
---
+++ 1DS-2026-JS/aula03.js
@@ -0,0 +1,20 @@
+let nome = "Hellen";
+
+if (nome == "Hellen") {
+    console.log("Voce digitou o nome correto!")    
+}else{
+    console.log("Voce digitoun outra coisa...")
+}
+
+let idade = 17;
+
+if (idade == 18) {
+    console.log("Voce é praticamente adulto...")
+}else if( idade < 18 ){
+    console.log("Voce ainda é de menor...")
+}else {
+    console.log('Voce é adulto...')
+}
+
+//FAÇA UMA VERIFICAÇÃO COM IF E ELSE 
+//DE UMA PESSOA SE ELA PODE OU NÃO VOTAR
